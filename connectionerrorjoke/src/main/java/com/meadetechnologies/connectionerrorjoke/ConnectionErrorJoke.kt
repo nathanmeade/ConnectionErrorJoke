0e5c51f1ba598421cae39f9b41d05a1709aa5337
nathanmeade <nathan.r.meade@gmail.com>
2023-03-08 16:34:49 -0500

    wip

--- connectionerrorjoke/src/main/java/com/meadetechnologies/connectionerrorjoke/ConnectionErrorJoke.kt
+++ connectionerrorjoke/src/main/java/com/meadetechnologies/connectionerrorjoke/ConnectionErrorJoke.kt
@@ -1,10 +1,20 @@
 package com.meadetechnologies.connectionerrorjoke
 
+import kotlin.random.Random
+
 class ConnectionErrorJoke {
 
+
+
     companion object {
-        fun getJoke() : String {
-            return "Looks like someone forgot to pay the internet bill!"
+        private val jokes = listOf<String>(
+            "Looks like someone forgot to pay the internet bill!",
+            "Did your dog chew through your ethernet cable?",
+            "Did you try to wash your wireless router?"
+        )
+        fun getJoke(): String {
+            val randomIndex = Random.nextInt(jokes.size)
+            return jokes[randomIndex]
         }
     }
 
