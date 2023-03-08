68c51d180775ac7d4c0e594f6719507839d6ef6f
nathanmeade <nathan.r.meade@gmail.com>
2023-03-08 14:24:39 -0500

    Initial commit

---
+++ connectionerrorjoke/src/main/java/com/meadetechnologies/connectionerrorjoke/ConnectionErrorJoke.kt
@@ -0,0 +1,11 @@
+package com.meadetechnologies.connectionerrorjoke
+
+class ConnectionErrorJoke {
+
+    companion object {
+        fun getJoke() : String {
+            return "Looks like someone forgot to pay the internet bill!"
+        }
+    }
+
+}
